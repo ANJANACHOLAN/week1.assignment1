5fa16f316554291513f419addfcee12a3c5e9b4f
ANJANACHOLAN <109015142+ANJANACHOLAN@users.noreply.github.com>
2022-07-12 19:09:04 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class OddNumbers {
+
+				public static void main(String[] args) {
+				int num = 20;
+				int i;
+				for (i = 1; i <= num; i++) {
+					if (i % 2 == 1) {
+						System.out.println(i);
+
+	}
+
+}
+				}
+}
